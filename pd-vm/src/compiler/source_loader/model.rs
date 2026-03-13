d62f539420c6a351e1afe25d3e104328cdbec4d4
fffonion <fffonion@gmail.com>
2026-03-14 02:22:18 +0800

    pd-vm: generics take 2

--- pd-vm/src/compiler/source_loader/model.rs
+++ pd-vm/src/compiler/source_loader/model.rs
@@ -24,12 +24,18 @@ pub(super) struct ModuleImport {
     pub(super) line: usize,
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub(super) struct ExportedFunctionSignature {
+    pub(super) arity: u8,
+    pub(super) type_params: Vec<String>,
+}
+
 #[derive(Default)]
 pub(super) struct ModuleCollectState {
     pub(super) visiting: Vec<PathBuf>,
     pub(super) seen: HashSet<PathBuf>,
     pub(super) units: Vec<ParsedUnit>,
-    pub(super) module_exports: HashMap<PathBuf, HashMap<String, u8>>,
+    pub(super) module_exports: HashMap<PathBuf, HashMap<String, ExportedFunctionSignature>>,
 }
 pub(super) struct ImportRewriteResult {
     pub(super) source: String,
